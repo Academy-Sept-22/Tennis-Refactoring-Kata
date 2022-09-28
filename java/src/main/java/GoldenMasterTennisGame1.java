9d813c05cbfd267678e23371be3a663cbfd25ab6
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-28 14:42:19 +0100

    Golden Master refactor setup

--- java/src/main/java/GoldenMasterTennisGame1.java
+++ java/src/main/java/GoldenMasterTennisGame1.java
@@ -1,33 +1,29 @@
 
-import 'dart:core';
+public class GoldenMasterTennisGame1 implements TennisGame {
 
-import 'TennisGame.dart';
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private String player1Name;
+    private String player2Name;
 
-class TennisGame1 implements TennisGame {
-
-    int _m_score1 = 0;
-    int _m_score2 = 0;
-    String _player1Name;
-    String _player2Name;
-
-    TennisGame1(String player1Name, String player2Name) {
-        this._player1Name = player1Name;
-        this._player2Name = player2Name;
+    public GoldenMasterTennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
-    void wonPoint(String playerName) {
+    public void wonPoint(String playerName) {
         if (playerName == "player1")
-            _m_score1 += 1;
+            m_score1 += 1;
         else
-            _m_score2 += 1;
+            m_score2 += 1;
     }
 
-    String getScore() {
+    public String getScore() {
         String score = "";
         int tempScore=0;
-        if (_m_score1==_m_score2)
+        if (m_score1==m_score2)
         {
-            switch (_m_score1)
+            switch (m_score1)
             {
                 case 0:
                         score = "Love-All";
@@ -41,12 +37,12 @@ class TennisGame1 implements TennisGame {
                 default:
                         score = "Deuce";
                     break;
-
+                
             }
         }
-        else if (_m_score1>=4 || _m_score2>=4)
+        else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = _m_score1-_m_score2;
+            int minusResult = m_score1-m_score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -56,8 +52,8 @@ class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = _m_score1;
-                else { score+="-"; tempScore = _m_score2;}
+                if (i==1) tempScore = m_score1;
+                else { score+="-"; tempScore = m_score2;}
                 switch(tempScore)
                 {
                     case 0:
